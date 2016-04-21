6226c6ca90f225dc91797134982312c85ff4400d
Umit Ates <umit.ates@ericsson.com>
2016-04-21 19:14:08 +0300

    Rehbere kisi ekleme fonksiyonelitesi eklendi

--- src/main/java/com/umitates/cl/contact/ContactController.java
+++ src/main/java/com/umitates/cl/contact/ContactController.java
@@ -1,13 +1,18 @@
 package com.umitates.cl.contact;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.umitates.cl.db.entity.ContactEntity;
 import com.umitates.cl.db.entity.UserEntity;
 import com.umitates.cl.db.repository.UserRepository;
 
@@ -17,15 +22,30 @@ public class ContactController {
 	@Autowired
 	private UserRepository userRepository;
  
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(value = {"/", "/welcome"}, method = RequestMethod.GET)
     public String welcomePage(ModelMap model) {
     	User user = (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     	UserEntity userEntity = getUserDetail(user.getUsername());
         
         model.addAttribute("contacts", userEntity.getContacts());
+        model.addAttribute("new_contact", new ContactEntity());
         
         return "welcome";
     }
+    
+	@RequestMapping(value = "/addContact", method = RequestMethod.POST)
+	public String addContact(@ModelAttribute(value = "new_contact") ContactEntity newContact, BindingResult result){
+		User user = (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    	UserEntity userEntity = getUserDetail(user.getUsername());
+		
+		newContact.setId(UUID.randomUUID().toString());
+		
+		userEntity.getContacts().add(newContact);
+		
+		userRepository.save(userEntity);
+		
+		return "redirect:/welcome";
+	}
     
 	public UserEntity getUserDetail(String username) {
 		return userRepository.findByUsername(username);
--- src/main/webapp/WEB-INF/views/welcome.jsp
+++ src/main/webapp/WEB-INF/views/welcome.jsp
@@ -4,36 +4,56 @@
 
 <html>
 <head>
-    <title>Kisi Listesi</title>
+<title>Kisi Listesi</title>
 </head>
 <body>
- 
-<h1>Kisi Listesi Uygulamasina Hos Geldiniz</h1>
 
-<c:if  test="${!empty contacts}">
-<table class="data">
-<tr>
-	
-    <th>Isim</th>
-    <th>Soyisim</th>
-    <th>E-Posta</th>
-    <th>Telefon</th>    
-    <th>Islem</th>
-	
-</tr>
-<c:forEach items="${contacts}" var="contact">
-    <tr>
-   		
-        <td>${contact.name} </td>
-        <td>${contact.surname} </td>
-        <td>${contact.email} </td>
-        <td>${contact.phoneNumber} </td>
-        
-        <td><a href="deleteContact/${contact.id}">Sil</a></td>
-    </tr>
-</c:forEach>
-</table>
-</c:if>
+	<h1>Kisi Listesi Uygulamasina Hos Geldiniz</h1>
+
+	<form:form method="post" action="addContact" commandName="new_contact">
+		<table>
+			<tr>
+				<td>Name:</td>
+				<td><form:input path="name" /></td>
+			</tr>
+			<tr>
+				<td>Surname</td>
+				<td><form:input path="surname" /></td>
+			</tr>
+			<tr>
+				<td>Email</td>
+				<td><form:input path="email" /></td>
+			</tr>
+			<tr>
+				<td>Telefon</td>
+				<td><form:input path="phoneNumber" /></td>
+			</tr>
+			<tr>
+				<td colspan="2"><input type="submit" value="Ekle" /></td>
+			</tr>
+		</table>
+	</form:form>
+
+	<c:if test="${!empty contacts}">
+		<table class="data">
+			<tr>
+				<th>Isim</th>
+				<th>Soyisim</th>
+				<th>E-Posta</th>
+				<th>Telefon</th>
+				<th>Islem</th>
+			</tr>
+			<c:forEach items="${contacts}" var="contact">
+				<tr>
+					<td>${contact.name}</td>
+					<td>${contact.surname}</td>
+					<td>${contact.email}</td>
+					<td>${contact.phoneNumber}</td>
+					<td><a href="deleteContact/${contact.id}">Sil</a></td>
+				</tr>
+			</c:forEach>
+		</table>
+	</c:if>
 
 </body>
 </html>
